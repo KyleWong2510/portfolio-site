957507b10314363db4d42fb802e4a240ec2ee3ed
Kyle Wong <kylewong2510@gmail.com>
2020-12-28 17:23:43 -0700

    Write products query

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <h1>Projects</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const Projects = ({ data }) => {
+  const { allContentfulProject: { nodes: projects }} = data 
 
-export default SecondPage
+  const allProjects = projects.map(project => {
+    return project.title
+  })
+
+  return (
+    <Layout>
+      <SEO title="Projects" />
+      <h1>Projects</h1>
+      { allProjects }
+      <Link to="/">Go back to the homepage!</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulProject {
+      nodes {
+        title
+        abstract {
+          abstract
+        }
+        techStack
+        image {
+          fluid {
+            src
+          }
+        }
+        slug
+      }
+    }
+  }
+`
+
+export default Projects
