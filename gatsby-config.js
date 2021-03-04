d07045325a2e61017bd052cfa8cdc822228d8cfc
Kyle Wong <kylewong2510@gmail.com>
2021-03-04 14:31:01 -0700

    Change meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kyle Wong Dev Portfolio`,
+    description: `Professional Dev Portfolio`,
+    author: `Kyle Wong`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,19 +28,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/kwLogo.png`,
       },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: 'up8hxfo3v0be',
-        // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
