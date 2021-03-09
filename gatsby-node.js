dab99132726c1980ebcc54b539393c154465f87b
Kyle Wong <kylewong2510@gmail.com>
2021-03-09 14:01:33 -0700

    Restore templates folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,25 @@
-// const path = require('path')
+const path = require('path')
 
-// create pages dynamically
-// exports.createPages = async ({graphql, actions}) => {
-//   const {createPage} = actions
-//   const result = await graphql(`
-//     query GetProjects {
-//       projects: allContentfulProject {
-//         nodes {
-//           slug
-//         }
-//       }
-//     }
-//   `)
+//create pages dynamically
+exports.createPages = async ({graphql, actions}) => {
+  const {createPage} = actions
+  const result = await graphql(`
+    query GetProjects {
+      projects: allContentfulProject {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
 
-//   result.data.projects.nodes.forEach((project) => {
-//     createPage({
-//       path: `/projects/${project.slug}`,
-//       component: path.resolve('src/templates/project-template.js'),
-//       context: {
-//         slug: project.slug
-//       }
-//     })
-//   })
-// }
-
-const rimraf = require('rimraf');
-
-const PUBLIC_FOLDER = `${__dirname}/public`;
-
-exports.onPreBuild = () => {
-  // empty /public folder
-  rimraf.sync(PUBLIC_FOLDER + '/*');
-};
+  result.data.projects.nodes.forEach((project) => {
+    createPage({
+      path: `/projects/${project.slug}`,
+      component: path.resolve('src/templates/project-template.js'),
+      context: {
+        slug: project.slug
+      }
+    })
+  })
+}
---
+++ src/templates/project-template.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from '../components/Layout'
+import Image from 'gatsby-image'
+import TechBadge from '../components/TechBadge'
+import LinkButton from '../components/LinkButton'
+
+const ComponentName = ({ data: {project: {title, abstract: {abstract}, image: {fluid}, techStack, repoUrl, deployedUrl}}}) => {
+  const formattedTech = techStack.map(tech => <TechBadge tech={tech} />)
+
+  return (
+    <Layout>
+      <div className='single-project-top'>
+        <Link to='/projects' className='link-btn'>Back to Projects</Link>
+        <h1>{title}</h1>
+      </div>
+      <section className='project-info'>
+        <article className='project-info-left'>
+          <Image fluid={fluid} alt={title} className='project-info-img' />
+          <div className='badge-container'>
+            {formattedTech}
+          </div>
+          <div className='link-btn-container'>
+            {deployedUrl && <LinkButton linkText='Deployed Project' url={deployedUrl}/>}
+            <LinkButton linkText='Repo' url={repoUrl}/>
+          </div>
+        </article>
+        <article className='project-info-right'>
+          <p>{abstract}</p>
+        </article>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query GetSingleProject($slug: String) {
+    project: contentfulProject(slug: {eq: $slug}) {
+      title
+      abstract {
+        abstract
+      }
+      image {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+      techStack
+      repoUrl
+      deployedUrl
+      slug
+    }
+  }`
+
+
+export default ComponentName
