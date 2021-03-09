743098e2dac867176f407c596b07d6dca8374495
Kyle Wong <kylewong2510@gmail.com>
2021-03-09 13:58:30 -0700

    Add public folder rebuild code

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,12 @@
 //     })
 //   })
 // }
+
+const rimraf = require('rimraf');
+
+const PUBLIC_FOLDER = `${__dirname}/public`;
+
+exports.onPreBuild = () => {
+  // empty /public folder
+  rimraf.sync(PUBLIC_FOLDER + '/*');
+};
